10753d62b580d1abcbb2a9262f688d964788ba4b
tomas <tomomackio@gmail.com>
2025-05-04 20:49:35 +0300

    commit

--- rental/transport/transport_base.py
+++ rental/transport/transport_base.py
@@ -14,17 +14,14 @@ class Transport(ABC):
 
     @property
     def rental_status(self):
-        """Get current rental status."""
         return self._rental_status
 
     def rent(self):
-        """Rent the vehicle if available."""
         if self._rental_status == "rented":
             raise Exception(f"Vehicle {self.id} is already rented.")
         self._rental_status = "rented"
 
     def return_vehicle(self):
-        """Return the vehicle if it is currently rented."""
         if self._rental_status == "available":
             raise Exception(f"Vehicle {self.id} is already available.")
         self._rental_status = "available"
